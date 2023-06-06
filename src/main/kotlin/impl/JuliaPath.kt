b67afacb8ba09f662661c22c7a0d06b6957cf333
Luc Briand <34173752+Keluaa@users.noreply.github.com>
2023-06-06 22:37:41 +0200

    Improved library path finding

--- src/main/kotlin/impl/JuliaPath.kt
+++ src/main/kotlin/impl/JuliaPath.kt
@@ -1,6 +1,8 @@
 package com.keluaa.juinko.impl
 
 import java.io.FileNotFoundException
+import java.nio.file.Files
+import java.nio.file.Paths
 import java.util.concurrent.TimeUnit
 import java.util.logging.Level
 import java.util.logging.Logger
@@ -12,37 +14,44 @@ class JuliaPath {
 
         private const val JULIA_LIB_NAME = "julia"
         private const val JULIA_INTERNAL_LIB_NAME = "julia-internal"
+
         val JULIA_BIN_PATH: String
         val JULIA_INTERNAL_BIN_PATH: String
 
         init {
-            var path = pathFromProperties()
-            if (path == null)
-                path = pathFromEnv()
-            if (path == null)
-                path = pathFromCmdLine()
+            val options = arrayOf(
+                ::pathFromProperties,
+                ::pathFromBinDir,
+                ::pathFromJulia,
+                ::pathFromCmdLine
+            )
+
+            var path: String? = null
+            for (option in options) {
+                path = option()
+                if (path != null && checkForLibs(cleanPath(path)))
+                    break
+            }
+
             if (path == null)
                 throw FileNotFoundException("Could not get the path to the Julia bin dir from neither the ENV or command line.")
 
-            path = path.replace("\\\\", "/")
-                       .replace('\\', '/')
-
-            // On Windows 'System.mapLibraryName' does not add the 'lib' prefix of the Julia libs
-            val prefix = if (System.getProperty("os.name").startsWith("Windows")) "lib" else ""
-
-            JULIA_BIN_PATH = path + '/' + prefix + System.mapLibraryName(JULIA_LIB_NAME)
-            JULIA_INTERNAL_BIN_PATH = path + '/' + prefix + System.mapLibraryName(JULIA_INTERNAL_LIB_NAME)
+            val libs = libsPaths(cleanPath(path))
+            JULIA_BIN_PATH = libs.first
+            JULIA_INTERNAL_BIN_PATH = libs.second
         }
 
         private fun pathFromProperties(): String? = System.getProperty("juinko.julia_path", null)
 
-        private fun pathFromEnv(): String? {
+        private fun pathFromBinDir(): String? {
             val jl_bindir = System.getenv("JULIA_BINDIR")
-            if (jl_bindir != null) return jl_bindir
+            if (jl_bindir != null) return Paths.get(jl_bindir, "../lib").toString()
+            return null
+        }
 
+        private fun pathFromJulia(): String? {
             val jl_exe = System.getenv("JULIA")
-            if (jl_exe != null) return Path(jl_exe).parent.toString()
-
+            if (jl_exe != null) return Paths.get(Paths.get(jl_exe).parent.toString(), "../lib").toString()
             return null
         }
 
@@ -70,6 +79,26 @@ class JuliaPath {
             path = path.trim()
             path = path.substring(1, path.length - 1) // Remove the quotes
             return path
+        }
+
+        private fun cleanPath(path: String): String {
+            return path.replace("\\\\", "/")
+                       .replace('\\', '/')
+        }
+
+        private fun libsPaths(path: String): Pair<String, String> {
+            // On Windows 'System.mapLibraryName' does not add the 'lib' prefix of the Julia libs
+            val prefix = if (System.getProperty("os.name").startsWith("Windows")) "lib" else ""
+
+            val libJuliaPath = path + '/' + prefix + System.mapLibraryName(JULIA_LIB_NAME)
+            val libJuliaInternalPath = path + '/' + prefix + System.mapLibraryName(JULIA_INTERNAL_LIB_NAME)
+
+            return Pair(libJuliaPath, libJuliaInternalPath)
+        }
+
+        private fun checkForLibs(path: String): Boolean {
+            val libs = libsPaths(path)
+            return Files.exists(Path(libs.first)) && Files.exists(Path(libs.second))
         }
     }
 }
